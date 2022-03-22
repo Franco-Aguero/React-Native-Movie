421f2dc321d56c87ed1b2b1bb5d1e1c696a02dc8
Franco-Aguero <aguerofranco2002@gmail.com>
2022-03-21 23:49:59 -0300

    navigation bottom part 1

--- navigations/Navigation.js
+++ navigations/Navigation.js
@@ -1,39 +1,86 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import { Icon } from 'react-native-elements';
+import { Image } from 'react-native-elements';
 import Home from '../screens/Home';
 import Favorites from '../screens/Favorites';
+import { StyleSheet } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 
 export default function Navigation() {
+
+    const iconReturn = (route, color) => {
+        let iconName = {
+            home: "home-outline",
+            favorites: "heart-outline"
+        };
+        return (<Icon
+            type="material-community"
+            name={ iconName[route.name] }
+            fontSize={22}
+            color={color}  
+        />)
+    } 
+/* tabBarOptions:{
+                inactiveTintColor: "white",
+                activeTintColor: "red",
+            } */
   return (
     <NavigationContainer>
-        <Tab.Navigator screenOptions={{
-            tabBarLabelStyle: { fontSize: 12 },
-            tabBarItemStyle: { width: 100 },
-            tabBarStyle: { backgroundColor: 'powderblue' },
-            tabBarActiveTintColor:{ color: "white" },
-        }}
+        <Tab.Navigator
+        tabBarPosition="top"
+        initialRouteName="favorites"
+        tabBarShowLabel={false}
+        screenOptions={ ({ route }) => ({
+            tabBarIcon: ({ color }) => iconReturn(route, color),
+            tabBarActiveTintColor:"red",
+            tabBarInactiveTintColor:"white",
+            /*headerShown: false, = remove bar header
+            tabBarShowLabel:false, = show name of screen in navbar bottom*/
+            /* headerTitle:false,
+            headerTitleStyle:false,
+            headerShadowVisible:false,
+             */
+
+            
+            headerTitle:"Netflix",
+            /* headerTitle: (props) => ( // App Logo
+            <Image
+                style={{ width: 100, height: 25 }}
+                source={require('../assets/netflix.png')}
+                resizeMode='contain'
+            />
+            ), */
+              
+            headerStyle:styles.container,
+            
+            tabBarStyle:{ backgroundColor:"blue", borderTopLeftRadius:10, borderTopRightRadius:10}/* is bar Bottom */
+        })}
+            
     >
             <Tab.Screen 
                 name="home"
                 component={Home}
-                options={{ 
-                    title: "Home"
-                }}
+                
             />
             <Tab.Screen 
                 name="favorites"
                 component={Favorites}
-                options={{ 
-                    title: "Favoritos",
-                }}
+                
             />
             
         </Tab.Navigator>
     </NavigationContainer>
   )
 }
+const styles = StyleSheet.create({
+    container: {
+      display:"flex",
+      width:"100%",
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'yellow',
+    }});
--- screens/Favorites.js
+++ screens/Favorites.js
@@ -1,11 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Button } from 'react-native-elements';
 
-export default function Favorites() {
+export default function Favorites({navigation}) {
   return (
     <View>
       <Text>Welcome to the</Text>
       <Text>Favorites</Text>
+      <Button
+      title="Go somewhere"
+      onPress={() => {
+        // Navigate using the `navigation` prop that you received
+        navigation.navigate("home");
+      }}
+    />
+
     </View>
   )
 }
